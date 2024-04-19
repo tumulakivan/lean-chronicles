7818aa45cfab86ccaa7815d2907db352fc54cb31
Ivan Tumulak <ist812002@gmail.com>
2024-04-19 20:23:21 +0800

    black screen for now

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("The Mockingjay - Chronicles of Lean");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
